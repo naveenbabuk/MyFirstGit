9bf51639a75450256320d6266a9cc4fcba21f1aa
naveenbabukurella <naveenbabukurella@192.168.0.34>
2017-12-22 15:12:13 -0800

    Initial Commit

---
+++ MyFirstGit/src/com/javagain/Main.java
@@ -0,0 +1,19 @@
+package com.javagain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesAllData sd = new SalesAllData();
+		displaySalesGreetings();
+		sd.display();
+		
+
+	}
+	
+	public static void displaySalesGreetings() {
+		System.out.println("Welcome to sales data");
+		System.out.println("this is sales app.");
+	}
+
+}
---
+++ MyFirstGit/src/com/javagain/SalesAllData.java
@@ -0,0 +1,7 @@
+package com.javagain;
+
+public class SalesAllData {
+	public void display() {
+		System.out.println("TODO: Sales All Data");
+	}
+}
